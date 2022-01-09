ea1a9da3af901f6f52a92f33287a53801d1fe686
xhl-study <2713697048@qq.com>
2022-01-09 17:01:11 +0800

    .

--- lib/extension/date_time_format.dart
+++ lib/extension/date_time_format.dart
@@ -1,95 +1,89 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/services.dart';
-import 'package:study_kt_swift/widgets/date_format.dart';
-
 /// 时间格式化 根据dayjs更改过来的简易版本
+
+///  格式化字典 部分未实现
+///  var matches = {
+///     YY: String(this.$y).slice(-2),
+///     YYYY: this.$y,
+///     M: $M + 1,
+///     MM: Utils.s($M + 1, 2, '0'),
+///     MMM: getShort(locale.monthsShort, $M, months, 3),
+///     MMMM: getShort(months, $M),
+///     D: this.$D,
+///     DD: Utils.s(this.$D, 2, '0'),
+///     d: String(this.$W),
+///     dd: getShort(locale.weekdaysMin, this.$W, weekdays, 2),
+///     ddd: getShort(locale.weekdaysShort, this.$W, weekdays, 3),
+///     dddd: weekdays[this.$W],
+///     H: String($H),
+///     HH: Utils.s($H, 2, '0'),
+///     h: get$H(1),
+///     hh: get$H(2),
+///     a: meridiemFunc($H, $m, true),
+///     A: meridiemFunc($H, $m, false),
+///     m: String($m),
+///     mm: Utils.s($m, 2, '0'),
+///     s: String(this.$s),
+///     ss: Utils.s(this.$s, 2, '0'),
+///     SSS: Utils.s(this.$ms, 3, '0'),
+///     Z: zoneStr // 'ZZ' logic below
+///   };
+
 extension DateTimeFormat on DateTime {
   Map<String, dynamic> get _dateMap => {
         "YY": year.toString().substring(2),
         "YYYY": year,
         "M": month,
         "MM": DayUtil.preFixNum(month),
-        // "MMM": month,
-        //"MMMM": month,
         "D": day,
         "DD": DayUtil.preFixNum(day),
-        //'d': day,
-        //'dd': day,
-        //'ddd': day,
         'H': hour,
         'HH': DayUtil.preFixNum(hour),
         'h': hour % 12,
         'hh': DayUtil.preFixNum(hour % 12),
-        // 'a': hour,
-        // 'A': hour,
         'm': minute,
         'mm': DayUtil.preFixNum(minute),
         's': second,
         'ss': DayUtil.preFixNum(second),
-        // 'SSS': second,
-        'ms': microsecond,
-        'Z': millisecondsSinceEpoch,
-        'ZZ': microsecondsSinceEpoch,
+        'ms': millisecond,
+        'mss': DayUtil.preFixNum(millisecond, preFixLen: 3),
+        'Z': millisecondsSinceEpoch, //时间戳 毫秒
+        'ZZ': microsecondsSinceEpoch, //时间戳 微秒
       };
-
-  //  var matches = {
-  //       YY: String(this.$y).slice(-2),
-  //       YYYY: this.$y,
-  //       M: $M + 1,
-  //       MM: Utils.s($M + 1, 2, '0'),
-  //       MMM: getShort(locale.monthsShort, $M, months, 3),
-  //       MMMM: getShort(months, $M),
-  //       D: this.$D,
-  //       DD: Utils.s(this.$D, 2, '0'),
-  //       d: String(this.$W),
-  //       dd: getShort(locale.weekdaysMin, this.$W, weekdays, 2),
-  //       ddd: getShort(locale.weekdaysShort, this.$W, weekdays, 3),
-  //       dddd: weekdays[this.$W],
-  //       H: String($H),
-  //       HH: Utils.s($H, 2, '0'),
-  //       h: get$H(1),
-  //       hh: get$H(2),
-  //       a: meridiemFunc($H, $m, true),
-  //       A: meridiemFunc($H, $m, false),
-  //       m: String($m),
-  //       mm: Utils.s($m, 2, '0'),
-  //       s: String(this.$s),
-  //       ss: Utils.s(this.$s, 2, '0'),
-  //       SSS: Utils.s(this.$ms, 3, '0'),
-  //       Z: zoneStr // 'ZZ' logic below
-  //     };
 
   /// format [DateTime] to string
   String format(String formatStr) {
-    // if (formatStr.isEmpty) throw DateTimeFormatException('formatStr is null');
     return formatStr.replaceAllMapped(DayUtil.regexpFormat, (Match match) {
       return _dateMap[formatStr.substring(match.start, match.end)].toString();
     });
   }
 
-  /// tryFormat [DateTime] to string
-  // String tryFormat(String formatStr) {
-  //   try {
-  //     return format(formatStr);
-  //   } on DateTimeFormatException catch (e) {
-  //     return e.toString();
-  //   }
-  // }
-
-  /// get date week
-  int getWeek() {
-    return weekday;
+  /// format [fms] to DateTime？
+  static DateTime? tryParse(String fms) {
+    try {
+      final fs = fms.replaceAllMapped(DayUtil.regexpTimeNum, (Match match) {
+        return DayUtil.preFixNum(fms.substring(match.start, match.end));
+      });
+      return DateTime.parse(fs);
+    } on FormatException {
+      return null;
+    }
   }
+
+  /// 获取 当前月 有多少天
+  int get days => DateTime(year, month + 1, 0).day;
 }
 
 class DayUtil {
-  //format regex
+  //格式化 日期 正则
   static final RegExp regexpFormat = RegExp(
-      r"\[([^\]]+)]|Y{1,4}|M{1,4}|D{1,2}|d{1,4}|H{1,2}|h{1,2}|a|A|\bms\b|m{1,2}|s{1,2}|Z{1,2}|SSS");
+      r"\[([^\]]+)]|Y{1,4}|M{1,4}|D{1,2}|d{1,4}|H{1,2}|h{1,2}|a|A|\bmss\b|\bms\b|m{1,2}|s{1,2}|Z{1,2}|SSS");
 
-  //增加数字前缀
+  /// 日期 数字匹配 正则
+  static final RegExp regexpTimeNum = RegExp(r'\d+');
+
+  //增加 数字前缀
   static dynamic preFixNum(
-    int num, {
+    dynamic num, {
     int preFixLen = 2,
     String preFix = '0',
   }) {
@@ -99,15 +93,3 @@ class DayUtil {
         num.toString();
   }
 }
-
-// class DateTimeFormatException implements Exception {
-//   final String message;
-//   final int code;
-//
-//   DateTimeFormatException(this.message, {this.code = -1});
-//
-//   @override
-//   String toString() {
-//     return 'message:$message code:$code';
-//   }
-// }
--- lib/widgets/date_format.dart
+++ lib/widgets/date_format.dart
@@ -1,11 +1,11 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:study_kt_swift/extension/index.dart';
 
 class DateFormat extends StatelessWidget {
   DateFormat({Key? key}) : super(key: key);
   DateTime now = DateTime.now();
-  DateTime? dtf = DateTime.tryParse("2021-02-12 15:30:36.232");
+  // DateTime? dtf = DateTimeFormat.tryParse("2019-1-1T1:3:5.5638175+08:00");
+  DateTime? dtf = DateTimeFormat.tryParse("2022-01-09T00:47:22.535Z");
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -17,7 +17,9 @@ class DateFormat extends StatelessWidget {
         Text("当前 时间${now.format('HH:mm:ss')}"),
         Text("当前 时间戳${now.format('Z')}毫秒"),
         Text("当前 时间戳${now.format('ZZ')}微秒"),
-        Text("dtf:${dtf?.format("YYYY年MM月DD日")}"),
+        Text("dtf:${dtf?.format("YYYY年MM月DD日 HH:mm:ss.mss")}"),
+        Text("dtf 当前${dtf?.month}月，有多少${dtf?.days}天"),
+        Text("dtf 当前${dtf?.month}月，有多少${dtf?.days}天" r' cccss'),
       ],
     );
   }
